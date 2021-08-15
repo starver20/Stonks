9ccb4d5661b81f2b03be51ce6090030452fc936c
amarnarute <amarnarute2002@gmail.com>
2021-08-16 02:14:59 +0530

    Style: Different color on profit and loss

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const quantityEl = document.querySelector('#quantity');
 const currentPriceEl = document.querySelector('#current_price');
 const tradeBtn = document.querySelector('#trade_btn');
 const msgBox = document.querySelector('#msg_box');
+const backgroundEl = document.querySelector('#bckgrnd_div');
 
 const calculate = () => {
   const initialPrice = initialPriceEl.value;
@@ -25,6 +26,7 @@ const calculate = () => {
 
   if (priceDiff === 0) {
     setMsg(`You have not gained or lost any money.`);
+    backgroundEl.style.backgroundColor = 'var(--primary-color)';
   } else if (priceDiff > 0) {
     //profit
     let profit = (priceDiff * qty).toFixed(2);
@@ -32,6 +34,8 @@ const calculate = () => {
     setMsg(
       `Yay!! You made a profit of ${profit} with profit percentage of ${profitPercent} `
     );
+
+    backgroundEl.style.backgroundColor = '#00B300';
   } else {
     //loss
     let loss = (Math.abs(priceDiff) * qty).toFixed(2);
@@ -39,6 +43,7 @@ const calculate = () => {
     setMsg(
       `Oops!! You made a loss of ${loss} with loss percentage of ${lossPercent} `
     );
+    backgroundEl.style.backgroundColor = '#F60000';
   }
 };
 
